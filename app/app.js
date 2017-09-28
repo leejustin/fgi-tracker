5fe185be4c17cf9dc04cb75bc1b495be95f724ee
Justin <leejustin@ucla.edu>
2017-09-27 21:43:09 -0700

    Update list api

--- app/app.js
+++ app/app.js
@@ -31,67 +31,85 @@ module.exports = function () {
         var start = new Date(req.params.id);
 
         var end = new Date();
-        if ( isNaN(start.valueOf())) {
+        if (isNaN(start.valueOf())) {
             //TODO: create and return errorDto
             res.write("error")
             res.end();
-        } else {
-            //TODO: clean up into helper
-            start.setUTCHours(0);
-            start.setMinutes(0);
-
-            end.setDate(start.getDate() +1);
-            end.setUTCHours(0);
-            end.setMinutes(0);
-
-            FgiRecord.find({
-                _id: {
-                    $gte: start,
-                    $lt: end
-                }
-            }, function(err, records) {
-                var dateVal = null;
-                var closeVal = null;
-
-                //TODO: error DTO
-                if (err) throw err;
-                
-                if (records.length > 0) {
-                    latestIndex = records.length - 1;
-                    var dateVal = records[latestIndex]._id;
-                    var closeVal = records[latestIndex].now;
-                }
-
-                res.write(JSON.stringify(new FgiRecordDto(dateVal, closeVal)));
-                res.end();
-            });
+            return next();
         }
-    });
 
-    server.get(baseUrl + '/records', function (req, res, next) {
+        //TODO: clean up into helper
+        start.setUTCHours(0);
+        start.setMinutes(0);
 
-        var start = req.query.start;
-        var end = req.query.end;
-        var date = req.query.date;
-
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-
-        console.log(req.query);
-        var start = new Date("2017-9-19");
-        var end = new Date();
+        end.setDate(start.getDate() + 1);
+        end.setUTCHours(0);
+        end.setMinutes(0);
 
         FgiRecord.find({
             _id: {
-              $gte: start,
-              $lt: end
+                $gte: start,
+                $lt: end
             }
-          }, function(err, record) {
+        }, function (err, records) {
+            var dateVal = null;
+            var closeVal = null;
+
+            //TODO: error DTO
             if (err) throw err;
-            res.write(JSON.stringify(record));
+
+            if (records.length > 0) {
+                latestIndex = records.length - 1;
+                var dateVal = records[latestIndex]._id;
+                var closeVal = records[latestIndex].now;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+            res.write(JSON.stringify(new FgiRecordDto(dateVal, closeVal)));
+            res.end();
+            return next();
+        });
+
+    });
+
+    server.get(baseUrl + '/records/', function (req, res, next) {
+
+        var start = new Date(req.query.start);
+        var end = new Date(req.query.end);
+
+        if (isNaN(start.valueOf()) || isNaN(end.valueOf())) {
+            //TODO: create and return errorDto
+            res.write("error")
+            res.end();
+            return next();            
+        }
+
+        //Clean up dates
+        start.setUTCHours = 0;
+        start.setMinutes = 0;
+        end.setUTCHours = 0;
+        end.setUTCMinutes = 0;
+
+        FgiRecord.find({
+            _id: {
+                $gte: start,
+                $lt: end
+            }
+        }, function (err, records) {
+            if (err) throw err;
+            console.log(records);
+            var resVal = [];
+
+            for (record in records) {
+                resVal.push(new FgiRecordDto(record._id, record.now));
+            }
+            
+            res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });            
+            res.write(JSON.stringify(resVal));
             res.end();
 
             return next();
-          });
+        });
     });
 
     server.listen(8080, function () {
